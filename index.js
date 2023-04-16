f20b183546a84ef68dc4ec07cc464a29317693d0
Maral <maral.mamedovna@gmail.com>
2023-04-17 00:39:11 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+ name: 'Sam',
+ streetAdress: '11 Broadway'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+ const updatedEmployee = { ...employee };
+ updatedEmployee[key] = value;
+ return updatedEmployee;
+}
+console.log(updateEmployeeWithKeyAndValue(employee, 'streetAdress', '12 Broadway'));
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+ employee[key] = value;
+ return employee;
+}
+console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', 'Maral'))
+
+function deleteFromEmployeeByKey(employee, key) {
+ const employeeByKey = { ...employee };
+ delete employeeByKey[key];
+ return employeeByKey;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+ delete employee[key];
+ return employee;
+}
